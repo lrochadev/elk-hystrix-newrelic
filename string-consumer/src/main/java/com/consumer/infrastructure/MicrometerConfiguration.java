7270a8bc0f2045d667ec4f0bd3dde0272f2f1ffb
Leonardo Rocha <lrochadev@gmail.com>
2022-04-23 19:19:40 -0300

    feat: adding micrometer lib to capture metrics and expose.

---
+++ string-consumer/src/main/java/com/consumer/infrastructure/MicrometerConfiguration.java
@@ -0,0 +1,16 @@
+package com.consumer.infrastructure;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MicrometerConfiguration {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+}
