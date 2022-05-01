d6b830bb36b98ceea11eea612900cd5be0e940f3
Leonardo Rocha <lrochadev@gmail.com>
2022-05-01 17:38:32 -0300

    feat: update gateway routes to work with service name.

--- spring-cloud-gateway/src/main/java/com/gateway/springcloudgateway/RoutesConfiguration.java
+++ spring-cloud-gateway/src/main/java/com/gateway/springcloudgateway/RoutesConfiguration.java
@@ -1,6 +1,5 @@
 package com.gateway.springcloudgateway;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,22 +8,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RoutesConfiguration {
 
-    @Value("${consumer.api.url}")
-    private String consumerAPI;
-
-    @Value("${producer.api.url}")
-    private String producerAPI;
-
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/consumer/**")
                         .filters(f -> f.retry(5))
-                        .uri(consumerAPI)
+                        .uri("lb://string-consumer")
                 )
                 .route(r -> r.path("/producer/**")
                         .filters(f -> f.addResponseHeader("X-AnotherHeader", "passou_no_gtw"))
-                        .uri(producerAPI)
+                        .uri("lb://string-producer")
                 )
                 .build();
     }
