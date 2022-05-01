ad5d5281ece4f96bdfe164c0c49ba3d01561729e
Leonardo Rocha <lrochadev@gmail.com>
2022-05-01 17:36:18 -0300

    feat: test eureka discovery by application name.

--- spring-cloud-gateway/src/main/java/com/gateway/springcloudgateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/gateway/springcloudgateway/SpringCloudGatewayApplication.java
@@ -1,13 +1,34 @@
 package com.gateway.springcloudgateway;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
 
+    @Component
+    class DiscoveryClientExample implements CommandLineRunner {
+
+        private final DiscoveryClient discoveryClient;
+
+        public DiscoveryClientExample(DiscoveryClient discoveryClient) {
+            this.discoveryClient = discoveryClient;
+        }
+
+        @Override
+        public void run(String... strings) {
+            discoveryClient.getInstances("STRING-CONSUMER").forEach((ServiceInstance s) -> System.out.println(ToStringBuilder.reflectionToString(s)));
+            discoveryClient.getInstances("STRING-PRODUCER").forEach((ServiceInstance s) -> System.out.println(ToStringBuilder.reflectionToString(s)));
+        }
+    }
 }
+
