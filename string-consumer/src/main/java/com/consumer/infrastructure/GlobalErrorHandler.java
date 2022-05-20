e388dfb4c9a0b991023389fafbb341a65915787e
Leonardo Rocha <lrochadev@gmail.com>
2022-05-20 14:12:46 -0300

    feat: adding global error handler.

---
+++ string-consumer/src/main/java/com/consumer/infrastructure/GlobalErrorHandler.java
@@ -0,0 +1,35 @@
+package com.consumer.infrastructure;
+
+import com.consumer.exceptions.ExercicioNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestControllerAdvice
+public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
+    }
+
+    @ExceptionHandler(ExercicioNotFoundException.class)
+    public final ResponseEntity<?> handlePlanetNotFoundException(ExercicioNotFoundException ex) {
+        return new ResponseEntity<>(Map.of("message", ex.getMessage()), NOT_FOUND);
+    }
+}
