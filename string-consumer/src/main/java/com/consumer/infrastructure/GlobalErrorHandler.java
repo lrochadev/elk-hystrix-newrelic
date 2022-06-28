71ac97ef5cccab9d3efc866b7816d4dc7c882448
Leonardo Rocha <lrochadev@gmail.com>
2022-06-27 21:13:06 -0300

    feat: removing exercises feature.

--- string-consumer/src/main/java/com/consumer/infrastructure/GlobalErrorHandler.java
+++ string-consumer/src/main/java/com/consumer/infrastructure/GlobalErrorHandler.java
@@ -1,19 +1,13 @@
 package com.consumer.infrastructure;
 
-import com.consumer.exceptions.ExercicioNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.Map;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
@@ -26,10 +20,5 @@ public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
-    }
-
-    @ExceptionHandler(ExercicioNotFoundException.class)
-    public final ResponseEntity<?> handlePlanetNotFoundException(ExercicioNotFoundException ex) {
-        return new ResponseEntity<>(Map.of("message", ex.getMessage()), NOT_FOUND);
     }
 }
